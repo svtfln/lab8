ec5a87d23f2ae001365e2aa09567fa8716e6e05c
svtfln <svetafilon78@gmail.com>
2022-01-09 17:02:32 +0300

    refactor and add new test

--- lab5/src/test/java/visitor/example/shapes/FactoryTest.java
+++ lab5/src/test/java/visitor/example/shapes/FactoryTest.java
@@ -21,9 +21,20 @@ public class FactoryTest {
     public void testNormalRectangleFactory() {
         Rectangle expectedResult = new Rectangle(2, 23, 15);
 
-        factory = new CircleFactory();
+        factory = new RectangleFactory();
 
         Rectangle acceptedResult = (Rectangle) factory.createNewShape(2, 23, 15);
+
+        Assert.assertEquals(expectedResult, acceptedResult);
+    }
+
+    @Test
+    public void testNormalDotFactory() {
+        Dot expectedResult = new Dot(1, 53, 18);
+
+        factory = new DotFactory();
+
+        Dot acceptedResult = (Dot) factory.createNewShape(1, 53, 18);
 
         Assert.assertEquals(expectedResult, acceptedResult);
     }
